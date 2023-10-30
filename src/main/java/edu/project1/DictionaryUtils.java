781e858b9ffdc669b6df550b34742fc3635b7036
artseroff <artseroff@gmail.com>
2023-10-30 18:22:16 +0300

    add reading from words.txt file and delete commented code

--- src/main/java/edu/project1/DictionaryUtils.java
+++ src/main/java/edu/project1/DictionaryUtils.java
@@ -1,9 +1,33 @@
 package edu.project1;
 
+import edu.project1.exceptions.InvalidWordException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DictionaryUtils {
-    private final static List<String> WORDS = List.of("причина", "инквизиция", "ворона", "фуксия");
+    private final static List<String> WORDS = new ArrayList<>();
+
+    static {
+        initWords();
+    }
+
+    public static void initWords() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        try (InputStream inputStream = classloader.getResourceAsStream("words.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                WORDS.add(line);
+            }
+
+        } catch (IOException | NullPointerException e) {
+            throw new InvalidWordException("Не удалось инициализировать словарь загадываемых слов");
+        }
+    }
 
     private DictionaryUtils() {
     }
@@ -11,8 +35,7 @@ public class DictionaryUtils {
     public static String randomWord() {
         int wordsSize = WORDS.size();
         int randomWordIndex = (int) (Math.random() * wordsSize);
-        String word = WORDS.get(randomWordIndex).trim().toLowerCase();
-        return word;
+        return WORDS.get(randomWordIndex).trim().toLowerCase();
     }
 
 }
